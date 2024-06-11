fd8430742b6e4814e80579c85ca93eb636ca2210
doerrza1 <doerrza1@msu.edu>
2024-06-11 14:01:25 -0400

    Added additional functionality to display

--- StreamMotion/cartesianstream.py
+++ StreamMotion/cartesianstream.py
@@ -21,7 +21,7 @@ client.connect()
 resp = client.send_init_pack()
 
 #Parses current cartesian position data from resp and prints the postitions
-current_car_data = resp[9:15] 
+current_car_data = resp[9:18] 
 print(['%.4f' % jnt for jnt in current_car_data]) 
 
 for i, value in enumerate(signal):
@@ -31,13 +31,13 @@ for i, value in enumerate(signal):
   
   if (i == 0):
     data = commandpack([1, 0, 0, car_data])  
-    print('COMMAND PACK SENT:', ['%.4f' % jnt for jnt in car_data])
-    print('Sent Seq No:', [1,0,1])
+    #print('COMMAND PACK SENT:', ['%.4f' % jnt for jnt in car_data])
+    #print('Sent Seq No:', [1,0,1])
     
   elif (i < len(signal)-1):
     data = commandpack([resp[2], 0, 0, car_data])
-    print('COMMAND PACK SENT:', ['%.4f' % jnt for jnt in car_data])
-    print('Sent Seq No:', [resp[2],0,1])
+    #print('COMMAND PACK SENT:', ['%.4f' % jnt for jnt in car_data])
+    #print('Sent Seq No:', [resp[2],0,1])
     
     if (i % 250 == 0):
       print("Velocity:")
@@ -52,19 +52,21 @@ for i, value in enumerate(signal):
 
   else:
     data = commandpack([resp[2], 1, 0, car_data])
-    print('COMMAND PACK SENT:', ['%.4f' % jnt for jnt in car_data])
-    print('Sent Seq No:', [resp[2], 1, 1])
+    #print('COMMAND PACK SENT:', ['%.4f' % jnt for jnt in car_data])
+    #print('Sent Seq No:', [resp[2], 1, 1])
 
 # Sends command pack and recieves status packet   
   resp = client.send_command_pack(data)
-  
+  if (i % 250 == 0):
+    display_cmd_pack(resp)
+
   # print('({:.2f}ms)'.format(1000*(time.time()-start_time)))
   
   # resp = explainRobData(resp) already called within send_command_pack
   
-  print('Received Seq No:', resp[2])
+  #print('Received Seq No:', resp[2])
   
-  current_car_data = resp[9:15]
+  current_car_data = resp[9:18]
 
 client.send_end_pack()
 print("End of stream")
--- StreamMotion/src/display.py
+++ StreamMotion/src/display.py
@@ -12,12 +12,22 @@ def display_cmd_pack(data):
     print("W (deg): ", data[12])
     print("P (deg): ", data[13])
     print("R (deg): ", data[14])
+    print("----------------------------")
 
 def display_limit_pack(data):
     print("Packet Type: ", data[0])
     print("Version Number: ", data[1])
     print("Axis Number: ", data[2])
-    print("Type of Limit: ", data[3])  #0 = vel, 1 = acc, 2 = jerk
+    
+    if (data[3] == 0):
+        limit = "Velocity (mm/s)"
+    elif (data[3] == 1):
+        limit = "Acceleration (mm/s^2)"
+    else:
+        limit = "Jerk (mm/s^3)"
+
+    print("Type of Limit: ", limit)
     print("Vmax (mm/s): ", data[4])
     print("Check Time (sec): ", data[5])
     print("Allowable limit: ", data[6])
+    print("----------------------------")
